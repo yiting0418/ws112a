77ad763a5a29a3883cf90253e98b9fa6c4a46551
yiting0418 <145081180+yiting0418@users.noreply.github.com>
2023-09-22 10:00:59 +0800

    Update 2.js

--- homework/2/2.js
+++ homework/2/2.js
@@ -1,55 +1,43 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
 const room = new Map();
-room.set("320", {
-  "教室": "322",
-  "功能": "多媒體教室",
-});
-room.set("319", {
-  "教室": "319",
-  "功能": "嵌入式實驗室",
-});
 
+room.set("319", {
+  id: "319",
+  name: "數位系統應用實驗室",
+});
+room.set("320", {
+  id: "320",
+  name: "多媒體實驗室",
+});
+room.set("321", {
+  id: "321",
+  name: "電腦網路實驗室",
+});
+room.set("322", {
+  id: "322",
+  name: "嵌入式實驗室",
+});
 const router = new Router();
 router
-  .get("", (context) => {
-    context.response.body = `
-    <html>
-        <body>
-        </body>
-    </html>`;
+  .get("/", (context) => {
+    context.response.body = "Hello world!";
   })
   .get("/nqu", (context) => {
-    context.response.body = `
-    <html>
-        <body>
-            <a href="https://www.nqu.edu.tw/">NQU</a>
-        </body>
-    </html>`;
+    context.response.body = `<html><body><a href="https://nqu.edu.tw"></a></body></html>`;
   })
   .get("/nqu/csie", (context) => {
-    context.response.body = `
-    <html>
-        <body>
-            <a href="https://csie.nqu.edu.tw/">NQU_CSIE</a>
-        </body>
-    </html>`;
+    context.response.body = `<html><body><a href="https://csie.nqu.edu.tw"></a></body></html>`;
   })
-  .get("/nqu/csie/:id", (context) => {
-    if (context.params && context.params.id && room.has(context.params.id)) {
-      context.response.body = Array.from(room.get(context.params.id));
+  .get("/nqu/room/:id", (context) => {
+    if (context.params && context.params.id && books.has(context.params.id)) {
+      context.response.body = books.get(context.params.id);
     }
-  })
-  .get("/to/nqu", (context) => {
-    context.response.redirect('https://www.nqu.edu.tw/')
-  })
-  .get("/to/nqu/csie", (context) => {
-    context.response.redirect('https://csie.nqu.edu.tw/')
   });
 
 const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-console.log("start at : http://127.0.0.1:8000");
+console.log('start at : http://127.0.0.1:8000')
 await app.listen({ port: 8000 });
